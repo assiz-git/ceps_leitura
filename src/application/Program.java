9d6f45c49453c1e96a8d6695121b30d1d7221097
Rogerio Assiz <44715540+assiz-git@users.noreply.github.com>
2019-01-15 17:25:43 -0200

    Add files via upload

---
+++ src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String path = "M:\\ID_221.479 - DP-2016-0040878 - Base de Perdas - TG\\Produção\\20181228\\A100A.G050.txt";
+		path="L:\\Rogerio\\Cursos\\Udemy\\Ceps\\ceps.csv";
+		BufferedReader br = null;
+		FileReader fr = null;
+		try {
+			//Integer linhas = 0;
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while (line != null) {
+				//linhas++;
+				//System.out.println(line);
+				if (line.contains(";")) {
+					//String[] campo = line.split("\\|");
+					String[] campo = line.split("\\;");
+					//for (int i=0;i<campo.length;i++) {
+						StringBuilder stb = new StringBuilder();
+						stb.append("{\n");
+						stb.append("  \"dados\": \n");
+						stb.append("    {\n");
+						stb.append("      \"cep\": \"" + campo[0] + "\",\n");
+						stb.append("      \"cidade\": \"" + campo[1] + "\",\n");
+						stb.append("      \"estado\": \"" + campo[2] + "\",\n");
+						stb.append("      \"bairro\": \"" + campo[3] + "\",\n");
+						stb.append("      \"logradouro\": \"" + campo[4] + "\",\n");
+						stb.append("      \"complemento\": \"" + campo[5] + "\"\n");
+						stb.append("    }\n");
+						stb.append("}\n\n");
+						System.out.println(stb);
+						//System.out.println("Campo " + i + ": " + campo[i]);
+					//}
+				}
+				line = br.readLine();
+			}
+			//System.out.println(linhas);
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
